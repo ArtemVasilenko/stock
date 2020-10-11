8e875abe1b929dc314d68d33969f3edf5a350ce1
Artem <artemvasilenko88@gmail.com>
2020-10-11 09:43:38 +0300

    work

--- stockPresent/ViewController.swift
+++ stockPresent/ViewController.swift
@@ -1,17 +1,28 @@
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var repeatButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var cameraImageView: UIImageView!
     
-    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSesssion: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var captureDevice : AVCaptureDevice!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) else { return }
+        
+        //        self.cameraView.addSubview(backCamera)
+        
+        
     }
-
-
+    
+    
 }
 
 
