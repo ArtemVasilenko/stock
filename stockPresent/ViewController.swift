7eef09d45b5e817bd62f91b038c3224239563071
Artem <artemvasilenko88@gmail.com>
2020-10-11 08:40:08 +0300

    Initial Commit

---
+++ stockPresent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockPresent
+//
+//  Created by Артем Василенко on 11.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
